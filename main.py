95b98bda2af986af303cd17a16c381cc4e38672b
Harsha <harshahm0130@gmail.com>
2024-06-13 17:48:36 +0530

    Commit-3

--- main.py
+++ main.py
@@ -10,13 +10,29 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
-def start_click():
-    count_down(1 * 60)
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        label.config(text="Long Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label.config(text="Short Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        label.config(text="Work Time", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -31,6 +47,13 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for i in range(work_sessions):
+            marks += "✔"
+        check_mark.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -38,8 +61,8 @@ window = Tk()
 window.title("Pomodoro🍅")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-timer = Label(text="Timer", font=(FONT_NAME, 45, "bold"), foreground=GREEN, bg=YELLOW)
-timer.grid(column=1, row=0)
+label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), foreground=GREEN, bg=YELLOW)
+label.grid(column=1, row=0)
 
 canvas = Canvas(width=202, height=226, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
@@ -47,13 +70,13 @@ canvas.create_image(102, 114, image=tomato_img)
 timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-start = Button(text="Start", highlightthickness=0, command=start_click)
+start = Button(text="Start", highlightthickness=0, command=start_timer)
 start.grid(column=0, row=2)
 
 reset = Button(text="Reset", highlightthickness=0)
 reset.grid(column=2, row=2)
 
-check = Label(text="✔", foreground=GREEN, bg=YELLOW)
-check.grid(column=1, row=3)
+check_mark = Label(foreground=GREEN, bg=YELLOW)
+check_mark.grid(column=1, row=3)
 
 window.mainloop()
