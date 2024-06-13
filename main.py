537dfa3b4733564fdf198269186b97ba6ad1e4dd
Harsha <harshahm0130@gmail.com>
2024-06-13 18:26:44 +0530

    Commit-4.1

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from tkinter import *
 import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+PURPLE = "#640D6B"
+PINK = "#B51B75"
+ORANGE = "#E65C19"
+YELLOW = "#F8D082"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -18,7 +18,7 @@ timer = None
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
-    label.config(text="Timer")
+    label.config(text="Timer", fg=PURPLE)
     check_mark.config(text="")
     global reps
     reps = 0
@@ -35,13 +35,13 @@ def start_timer():
 
     if reps % 8 == 0:
         count_down(long_break_sec)
-        label.config(text="Long Break", fg=RED)
+        label.config(text="Long Break", fg=PINK)
     elif reps % 2 == 0:
         count_down(short_break_sec)
         label.config(text="Short Break", fg=PINK)
     else:
         count_down(work_sec)
-        label.config(text="Work Time", fg=GREEN)
+        label.config(text="Work Time", fg=ORANGE)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -71,7 +71,7 @@ window = Tk()
 window.title("Pomodoro🍅")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), foreground=GREEN, bg=YELLOW)
+label = Label(text="Timer", font=(FONT_NAME, 40, "bold"), foreground=PURPLE, bg=YELLOW)
 label.grid(column=1, row=0)
 
 canvas = Canvas(width=202, height=226, bg=YELLOW, highlightthickness=0)
@@ -86,7 +86,7 @@ start.grid(column=0, row=2)
 reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset.grid(column=2, row=2)
 
-check_mark = Label(font=(FONT_NAME, 20, "bold"), foreground=GREEN, bg=YELLOW)
+check_mark = Label(font=(FONT_NAME, 20, "bold"), foreground=ORANGE, bg=YELLOW)
 check_mark.grid(column=1, row=3)
 
 window.mainloop()
