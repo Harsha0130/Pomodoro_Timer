589c6f6137a1b686cfc2debc5c46d1f29959a27a
Harsha <harshahm0130@gmail.com>
2024-06-13 16:21:37 +0530

    Commit-2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,22 +11,44 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_click():
+    count_down(5*60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro🍅")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer = Label(text="Timer", font=(FONT_NAME, 45, "bold"), foreground=GREEN, bg=YELLOW)
+timer.grid(column=1, row=0)
 
 canvas = Canvas(width=202, height=226, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(102, 114, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start = Button(text="Start", highlightthickness=0, command=start_click)
+start.grid(column=0, row=2)
+
+reset = Button(text="Reset", highlightthickness=0)
+reset.grid(column=2, row=2)
+
+check = Label(text="✔", foreground=GREEN, bg=YELLOW)
+check.grid(column=1, row=3)
 
 window.mainloop()
